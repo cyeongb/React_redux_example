f4b66c6098c27b8db711f2860c77467ba9157fa9
yeongbi choe <cyeongb@gmail.com>
2021-04-28 01:38:37 +0900

    2021-04-28 add css

--- src/containers/HomePage/usersList.jsx
+++ src/containers/HomePage/usersList.jsx
@@ -10,7 +10,25 @@ const UsersContainers = styled.div`
   justify-content: space-evenly; /*시작과 끝 사이의 공간을 모두 균등하게 배분함 */
 `;
 
-const UserImage = styled.div``;
+const UserWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserImage = styled.div`
+  width: 100px;
+  height: 100px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const UserName = styled.h3`
+  font-size: 20px;
+  color: rgb(214, 122, 127);
+  margin: 0;
+`;
 
 const stateSelector = createSelector(makeSelectUsers, (users) => ({
   users,
@@ -18,4 +36,25 @@ const stateSelector = createSelector(makeSelectUsers, (users) => ({
 
 export function UsersList(props) {
   const { users } = useSelector(stateSelector);
+
+  const isEmptyUsers = !users || (users && users.length === 0);
+  if (isEmptyUsers) {
+    return "No users";
+  }
+
+  return (
+    <UsersContainers>
+      {users.map((user, i) => (
+        <UserWrapper key={i}>
+          <UserImage>
+            <img src={user.avatar} alt="user_avatar" />
+          </UserImage>
+          <UserName>
+            {user.first_name}
+            {user.last_name}
+          </UserName>
+        </UserWrapper>
+      ))}
+    </UsersContainers>
+  );
 }
